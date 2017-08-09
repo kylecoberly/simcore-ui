c005376e64916878bb284ced6327df3e0e5d6a04
Chad Beninati <chad@redtempest.net>
2017-08-09 13:28:55 -0600

    Debugging postinstall.

--- post-install.sh
+++ post-install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+pwd
 
 # Remove all top-level files except for our pacakge.json
-find . -maxdepth 1 ! \(  -name 'package.json' \) -type f -exec rm -f {} +
+# find . -maxdepth 1 ! \(  -name 'package.json' \) -type f -exec rm -f {} +
 
 # Move all our source files to the root.
-cd src/components
+cd ./src/components
 mv * ../../
 
 # Remove all top-level directories.
 cd ../../
-find . -type d -exec rm -fr {} +
+# find . -type d -exec rm -fr {} +
