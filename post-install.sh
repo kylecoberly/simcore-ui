64901facd5a0ce6d88ddf70e9e6b03ecd3584cb1
Chad Beninati <chad@redtempest.net>
2017-08-09 13:39:02 -0600

    Setting up postinstall.

---
+++ post-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pwd
+
+# Remove all top-level files except for our pacakge.json
+# find . -maxdepth 1 ! \(  -name 'package.json' \) -type f -exec rm -f {} +
+
+# Move all our source files to the root.
+cd ./src/components
+mv * ../../
+
+# Remove all top-level directories.
+cd ../../
+# find . -type d -exec rm -fr {} +
