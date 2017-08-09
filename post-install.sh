ffd9765a08a23c42d3d3f77e6453f60d90d4f760
Chad Beninati <chad@redtempest.net>
2017-08-09 14:20:56 -0600

    Updated postinstall directory removal.

--- post-install.sh
+++ post-install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-echo 'start'
-pwd
 
 # Remove all top-level files except for our pacakge.json
 find ./ -maxdepth 1 ! \(  -name 'package.json' \) -type f -exec rm -f {} +
 
 # Move all our source files to the root.
 cd ./src/components
-echo 'into components'
-pwd
 mv * ../../
 
 # Remove all top-level directories.
 cd ../../
-echo 'up 2'
+echo 'remove directories'
 pwd
-# find ./ -type d -exec rm -fr {} +
+find -mindepth 1 -maxdepth 1 -type d -exec rm -r {} \;
