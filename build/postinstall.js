535c0ec1c66d087ffefa3738b7ba63809f487f00
Chad Beninati <chad@redtempest.net>
2017-08-10 15:10:29 -0600

    Some environment debugging.

--- build/postinstall.js
+++ build/postinstall.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 var shell = require('shelljs');
 
 var isIntegration = process.env.INTEGRATION;
+console.log(isIntegration);
+console.log(process.env);
 
 if (isIntegration && isIntegration === 'true') {
   if (shell.exec('bash post-install.sh').code !== 0) {
