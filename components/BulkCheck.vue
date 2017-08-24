da9b2e815e9da37f5a53f500dbdbd2deca125b76
Jase <jase@star29.com>
2017-08-24 17:15:57 -0600

    Updated bulkCheck to only manage its own state, not an entire list

--- components/BulkCheck.vue
+++ components/BulkCheck.vue
@@ -1,49 +1,48 @@
 <template lang="html">
-  <button class="sim-bulkcheck" :class="{active: shouldBeActive}" @click="toggleSelectedItems">
-    <SimIconText :icon="`${icon} fa-fw`"></SimIconText>
-    <SimIconText :icon="`${icon} fa-fw`"></SimIconText>
-    <SimIconText :icon="`${icon} fa-fw`"></SimIconText>
-  </button>
+    <button class="sim-bulkcheck" :class="{active: checkAll}" @click="toggleState">
+        <SimIconText :icon="`${icon} fa-fw`"></SimIconText>
+        <SimIconText :icon="`${icon} fa-fw`"></SimIconText>
+        <SimIconText :icon="`${icon} fa-fw`"></SimIconText>
+    </button>
 </template>
 
 <script>
-  import SimIconText from './IconText'
+    import SimIconText from './IconText'
 
-  export default {
-    name: 'sim-bulkcheck',
-    components: {
-      SimIconText,
-    },
-    props: {
-      items: {
-        type: Array,
-        default: [],
-        required: true,
-      },
-      icon: {
-        type: String,
-        default: 'fa-check'
-      }
-    },
-    data () {
-      return {
-        checkedItems: []
-      }
-    },
-    computed: {
-      shouldBeActive () {
-        return this.checkedItems.length > 0
-      },
-    },
-    methods: {
-      toggleSelectedItems () {
-        this.checkedItems = this.checkedItems.length === this.items.length ? [] : this.items.map(item => item.id)
-        this.$emit('toggle', this.checkedItems)
-      },
+    export default {
+        name: 'sim-bulkcheck',
+        components: {
+            SimIconText,
+        },
+        props: {
+            state: {
+                type: Boolean,
+                default: false
+            },
+            icon: {
+                type: String,
+                default: 'fa-check'
+            }
+        },
+        data () {
+            return {
+                checkAll: this.state
+            }
+        },
+        methods: {
+          toggleState ($event, forceState) {
+              this.checkAll = forceState ? forceState : !this.checkAll
+              this.$emit('toggle', this.checkAll)
+          },
+        },
+        watch: {
+            state (value) {
+                this.toggleState(null, value)
+            }
+        },
     }
-  }
 </script>
 
 <style lang="scss">
-  @import '../styles/bulkcheck';
+    @import '../styles/bulkcheck';
 </style>
