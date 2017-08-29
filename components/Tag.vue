bb97f0c699c986709822b4395a58ce97a174e451
Jase <jase@star29.com>
2017-08-29 12:20:09 -0600

    New modal component (in-progress), new tag component

---
+++ components/Tag.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <span class="sim-tag">
+      <span v-if="icon" class="fa" :class="icon"></span>
+      <span v-if="text">{{ text }}</span>
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'sim-tag',
+    props: {
+        icon: String,
+        text: [String, Number],
+    },
+  }
+</script>
+
+<style lang="scss">
+  @import '../styles/tag';
+</style>
