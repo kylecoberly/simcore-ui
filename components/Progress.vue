35b3ef446db6e3df4d8c9f9cd1cd2290912ec8ea
Jase <jase@star29.com>
2017-08-29 14:34:48 -0600

    New progress bar component

---
+++ components/Progress.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="sim-progress" :style="stylePercentComplete">
+      <div class="sim-progress__completed" :tooltip="bubbleText"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sim-progress',
+    props: {
+        percent: [Number, String],
+    },
+    computed: {
+        stylePercentComplete () {
+            return `--percent:${this.percent}%`
+        },
+        bubbleText () {
+            return `${this.percent}% Complete`
+        },
+    },
+  }
+</script>
+
+<style lang="scss">
+  @import '../styles/progress';
+</style>
