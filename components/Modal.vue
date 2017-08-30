9a0e37c423d3a4fec99f1396621849c62acf86a3
Jase <jase@star29.com>
2017-08-30 15:16:12 -0600

    Modal component updates

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,47 +1,51 @@
 <template lang="html">
-    <transition name="sim-modal">
-        <div class="sim-modal" :class="{active: shouldBeOpen}">
-            <div class="sim-modal__mask" v-if="shouldShowMask" @click="clickMask"></div>
-            <div class="sim-modal__wrapper">
-                <slot name="header"></slot>
-                <slot name="content"></slot>
-                <slot name="footer"></slot>
-                <div class="sim-modal__dismiss" v-if="shouldShowDismiss" @click="dismiss">&times;</div>
-            </div>
-        </div>
-    </transition>
+  <div class="sim-modal" :class="{active: shouldBeOpen}">
+    <div class="sim-modal__mask" v-if="shouldShowMask" @click="clickMask"></div>
+    <div class="sim-modal__wrapper">
+      <slot></slot>
+      <div class="sim-modal__dismiss" v-if="shouldShowDismiss" @click="dismiss">&times;</div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'sim-modal',
     props: {
-        shouldBeOpen: {
-            type: Boolean,
-            default: false,
-        },
-        shouldShowDismiss: {
-            type: Boolean,
-            default: true,
-        },
-        shouldShowMask: {
-            type: Boolean,
-            default: true,
-        },
-        canClickMask: {
-            type: Boolean,
-            default: true,
-        },
+      shouldBeOpen: {
+        type: Boolean,
+        default: false,
+      },
+      dismissTo: {
+        type: String,
+        default: '',
+      },
+      shouldShowDismiss: {
+        type: Boolean,
+        default: true,
+      },
+      shouldShowMask: {
+        type: Boolean,
+        default: true,
+      },
+      canClickMask: {
+        type: Boolean,
+        default: true,
+      },
     },
     methods: {
-        dismiss: function(){
-            this.$emit('toggle')
-        },
-        clickMask: function(){
-            if(this.canClickMask) {
-                this.dismiss()
-            }
+      dismiss: function(){
+        if (this.$router && this.dismissTo && typeof this.dismissTo === 'string') {
+          this.$router.push(this.dismissTo)
+        } else {
+          this.$root.closeModal()
         }
+      },
+      clickMask: function(){
+        if(this.canClickMask) {
+          this.dismiss()
+        }
+      }
     }
   }
 </script>
