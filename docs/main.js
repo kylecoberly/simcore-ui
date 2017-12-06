aba53b656f79988a1a09cd45f6c2ed0d4280c2fe
Jase <jase@star29.com>
2017-12-06 16:09:05 -0700

    include and initial stub for vuex

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import VueHighlightJS from 'vue-highlightjs'
 
 import App from './App'
@@ -10,10 +11,18 @@ import common from './mixins'
 Vue.config.productionTip = false
 
 // helpers
+Vue.use(Vuex)
 Vue.use(VueHighlightJS)
 
+window.moment = require('moment')
+
+const store = new Vuex.Store({
+  state: {
+    active_date: moment().format('YYYY-MM-DD'),
+  },
+})
+
 /* eslint-disable no-new, import/prefer-default-export */
-// export const Events = new Vue({})
 
 new Vue({
   el: '#app',
