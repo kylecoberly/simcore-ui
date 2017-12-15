86f396260632fa4b49858c78502f9ecf6c523364
Chad Beninati <chad.beninati@gmail.com>
2017-12-14 18:41:50 -0700

    Added a SlideWithAnEventForm template.

---
+++ components/SlideWithAnEventForm.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="sim-slide">
+
+    <header v-if="data.title || data.subtitle" class="sim-slide--header">
+      <h2 v-if="data.title" class="sim-slide--title">{{ data.title }}</h2>
+      <div v-if="data.subtitle" class="sim-slide--subtitle">{{ data.subtitle }}</div>
+    </header>
+
+    <div v-if="data.intro">
+      {{ data.intro }}
+    </div>
+
+    <div v-if="data.content"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['data'],
+  }
+</script>
+
+<style lang="scss">
+  @import '../styles/slide';
+</style>
