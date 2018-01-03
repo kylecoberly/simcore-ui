7099b29382d50397a745eacf70a937eabb8bb62c
Eric Thayer <eric.thayer594@gmail.com>
2018-01-03 14:40:02 -0700

    button demo refactor and styling changes

--- docs/pages/styles/Buttons.vue
+++ docs/pages/styles/Buttons.vue
@@ -1,16 +1,64 @@
 <template lang="html">
   <article>
-    <h1>{{ msg }}</h1>
-    <p>pushing them all</p>
+    <h2>{{ msg }}</h2>
+    <demobox :open="true">
+
+        <template slot="intro">
+          Normal / Default
+        </template>
+
+        <template slot="view">
+          <sim-button :button="SimButton"></sim-button>
+          <sim-button :button="SimButtonSecondary" class="secondary"></sim-button>
+          <sim-button :button="SimButtonActive" class="active"></sim-button>
+          <sim-button :button="SimCalendarButton" class="sim-calendar-button"></sim-button>
+          <sim-button :button="SimButtonPlain" class="sim-button__plain"></sim-button>
+          <sim-button :button="SimButtonDisabled" disabled></sim-button>&nbsp;
+          <SimIconText icon="fa-arrow-circle-right fa-lg"></SimIconText>
+        </template>
+
+        <template slot="html">
+          <pre v-highlightjs><code class="html">&lt;button class="sim-button">Primary&lt;/button></code></pre>
+          <br>
+          <pre v-highlightjs><code class="html">&lt;button class="sim-button secondary">Secondary&lt;/button></code></pre>
+          <br>
+          <pre v-highlightjs><code class="html">&lt;button class="sim-button active">Active&lt;/button></code></pre>
+          <br>
+          <pre v-highlightjs><code class="html">&lt;button class="sim-button sim-calendar-button">Calendar&lt;/button></code></pre>
+          <br>
+          <pre v-highlightjs><code class="html">&lt;button class="sim-button sim-button__plain">Text&lt;/button></code></pre>
+          <br>
+          <pre v-highlightjs><code class="html">&lt;span class="sim-icontext sim-icontext__plain">
+    &lt;span class="sim-icon fa fa-arrow-circle-right fa-lg" aria-hidden="true">&lt;/span>
+&lt;/span>
+</code></pre>
+        </template>
+
+      </demobox>
   </article>
 </template>
 
 <script>
+  import Demobox from '../../utility/Demobox'
+  import SimIconText from '../../../components/IconText'
+  import SimButton from '../../../components/Button'
+  
 export default {
   name: 'guide-buttons',
+  components: {
+    Demobox,
+    SimIconText,
+    SimButton,
+  },
   data() {
     return {
       msg: 'Buttons',
+      SimButton: 'Primary',
+      SimButtonSecondary: 'Secondary',
+      SimButtonActive: 'Active',
+      SimCalendarButton: 'Calendar',
+      SimButtonPlain: 'Text',
+      SimButtonDisabled: 'Disabled',
     }
   },
 }
--- docs/pages/styles/Typography.vue
+++ docs/pages/styles/Typography.vue
@@ -1,61 +1,63 @@
 <template lang="html">
   <article>
+
     <h2>{{ msg }}</h2>
+
     <demobox :open="true">
+
         <template slot="intro">
           Normal / Default
         </template>
+
         <template slot="view">
-          <sim-button :button="SimButton"></sim-button>
-          <sim-button :button="SimButtonSecondary" class="secondary"></sim-button>
-          <sim-button :button="SimButtonActive" class="active"></sim-button>
-          <sim-button :button="SimCalendarButton" class="sim-calendar-button"></sim-button>
-          <sim-button :button="SimButtonPlain" class="sim-button__plain"></sim-button>
-          <sim-button :button="SimButtonDisabled" disabled></sim-button>&nbsp;
-          <SimIconText icon="fa-arrow-circle-right fa-lg"></SimIconText>
+          <p class="sg-label">Headlines</p>
+          <h1><sim-type :type="h1"></sim-type></h1>
+          <h2><sim-type :type="h2"></sim-type></h2>
+          <h3><sim-type :type="h3"></sim-type></h3>
+          <h4><sim-type :type="h4"></sim-type></h4>
+          <h5><sim-type :type="h5"></sim-type></h5>
+          <h6><sim-type :type="h6"></sim-type></h6>
+          <p class="sg-label">Text</p>
+          <p>This is a paragraph. Consectetur adipisicing elit <strong>this is emphasized</strong>. Laboriosam enim saepe a, vitae rerum sapiente velit, <a href="#">this is a link</a> incidunt at eligendi illo blanditiis quasi. Amet sint eveniet alias quasi <u>this is underlined</u> nulla ad optio, quod necessitatibus numquam pariatur eaque, vitae <em>this is italicized</em> illum illo molestiae qui corrupti eum tenetur voluptas. Ex voluptate velit dolor eligendi.</p>
+          <p>Veritatis ad ea sint cum facilis, laborum quo ex impedit. <span class="text-highlight">Laborum sint velit dolores provident obcaecati doloribus natus officiis, repellendus aliquid nobis</span>, eum aperiam fugiat in incidunt itaque. Illo perferendis qui maxime exercitationem, dolore iusto. Odio ex nobis totam cupiditate adipisci, voluptas iusto maxime quas, eaque omnis enim temporibus.</p>
         </template>
+
         <template slot="html">
-          <pre v-highlightjs><code class="html">&lt;button class="sim-button">Primary&lt;/button></code></pre>
-          <br>
-          <pre v-highlightjs><code class="html">&lt;button class="sim-button secondary">Secondary&lt;/button></code></pre>
-          <br>
-          <pre v-highlightjs><code class="html">&lt;button class="sim-button active">Active&lt;/button></code></pre>
-          <br>
-          <pre v-highlightjs><code class="html">&lt;button class="sim-button sim-calendar-button">Calendar&lt;/button></code></pre>
-          <br>
-          <pre v-highlightjs><code class="html">&lt;button class="sim-button sim-button__plain">Text&lt;/button></code></pre>
-          <br>
-          <pre v-highlightjs><code class="html">&lt;span class="sim-icontext sim-icontext__plain">
-    &lt;span class="sim-icon fa fa-arrow-circle-right fa-lg" aria-hidden="true">&lt;/span>
-&lt;/span>
-</code></pre>
+          <pre v-highlightjs><code class="html">&lt;</code></pre>
         </template>
+
+        <template slot="js">
+          <pre v-highlightjs><code class="javascript"></code></pre>
+        </template>
+
       </demobox>
+
   </article>
 </template>
 
 <script>
   import Demobox from '../../utility/Demobox'
   import SimIconText from '../../../components/IconText'
-  import SimButton from '../../../components/Typography'
+  import SimType from '../../../components/Typography'
   
 export default {
-  name: 'guide-buttons',
+  name: 'guide-typography',
   components: {
     Demobox,
     SimIconText,
-    Typography,
+    SimType,
   },
   data() {
     return {
-      msg: 'Buttons',
-      SimButton: 'Primary',
-      SimButtonSecondary: 'Secondary',
-      SimButtonActive: 'Active',
-      SimCalendarButton: 'Calendar',
-      SimButtonPlain: 'Text',
-      SimButtonDisabled: 'Disabled',
+      msg: 'Typography',
+      h1: 'Headline h1',
+      h2: 'Headline h2',
+      h3: 'Headline h3',
+      h4: 'Headline h4',
+      h5: 'Headline h5',
+      h6: 'Headline h6',
     }
   },
 }
 </script>
+
