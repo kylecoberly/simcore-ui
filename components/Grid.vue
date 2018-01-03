e1b9b448e9decd549b6fbf433502a12f714dd1e8
Eric Thayer <eric.thayer594@gmail.com>
2018-01-03 16:48:10 -0700

    new Style Guide sections

---
+++ components/Grid.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div>{{ grid }}</div>
+</template>
+
+<script>
+  export default {
+    name: 'sim-grid',
+    props: ['grid']
+}
+</script>
+
+<style lang="scss">
+  @import '../styles/grid';
+</style>
+
---
+++ docs/pages/styles/Color.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <article>
+
+    <h2 class="library-section-title">{{ msg }}</h2>
+
+    <demobox :open="true">
+
+        <template slot="intro">
+          Normal / Default
+        </template>
+
+        <template slot="view">
+
+        </template>
+
+        <template slot="html">
+          <pre v-highlightjs><code class="html">&lt;</code></pre>
+        </template>
+
+        <template slot="js">
+          <pre v-highlightjs><code class="javascript"></code></pre>
+        </template>
+
+      </demobox>
+
+  </article>
+</template>
+
+<script>
+  import Demobox from '../../utility/Demobox'
+  import SimIconText from '../../../components/IconText'
+  import SimColor from '../../../components/Color'
+  
+export default {
+  name: 'guide-color',
+  components: {
+    Demobox,
+    SimIconText,
+    SimColor,
+  },
+  data() {
+    return {
+      msg: 'Color',
+    }
+  },
+}
+</script>
