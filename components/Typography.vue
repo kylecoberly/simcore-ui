7e74e304d39b14e584686fae6c743307f6efeed5
Eric Thayer <eric.thayer594@gmail.com>
2018-01-03 16:43:42 -0700

    Added Typography section under Style Guide

---
+++ components/Typography.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <span>
+    {{ type }}
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'sim-type',
+    props: ['type']
+}
+</script>
+
+<style lang="scss">
+  @import '../styles/typography';
+</style>
+
--- docs/pages/styles/Typography.vue
+++ docs/pages/styles/Typography.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <article>
 
-    <h2>{{ msg }}</h2>
+    <h2 class="library-section-title">{{ msg }}</h2>
 
     <demobox :open="true">
 
@@ -10,20 +10,32 @@
         </template>
 
         <template slot="view">
-          <p class="sg-label">Headlines</p>
-          <h1><sim-type :type="h1"></sim-type></h1>
-          <h2><sim-type :type="h2"></sim-type></h2>
-          <h3><sim-type :type="h3"></sim-type></h3>
-          <h4><sim-type :type="h4"></sim-type></h4>
-          <h5><sim-type :type="h5"></sim-type></h5>
-          <h6><sim-type :type="h6"></sim-type></h6>
-          <p class="sg-label">Text</p>
+          <p class="library-label">Typeface</p>
+          <div class="h3 fw300 mb-sm">Montserrat 300</div>
+          <div class="h3 fw500 mb-sm">Montserrat 500</div>
+          <div class="h3 fw700 mb-sm">Montserrat 700</div>
+          <p class="library-label">Headlines</p>
+          <h1><sim-type :type="h1"></sim-type><div class="demo-info"><p>font-size: 3.25rem</p>
+          <p>font-weight: 700</p><p>line-height: 1.1</p></div></h1>
+          <h2><sim-type :type="h2"></sim-type><div class="demo-info"><p>font-size: 2.5rem</p>
+          <p>font-weight: 700</p><p>line-height: 1.4</p></div></h2>
+          <h3><sim-type :type="h3"></sim-type><div class="demo-info"><p>font-size: 2rem</p>
+          <p>font-weight: 700</p><p>line-height: 1.4</p></div></h3>
+          <h4><sim-type :type="h4"></sim-type><div class="demo-info"><p>font-size: 1.5rem</p>
+          <p>font-weight: 500</p><p>line-height: 1.4</p></div></h4>
+          <h5><sim-type :type="h5"></sim-type><div class="demo-info"><p>font-size: 1.25rem</p>
+          <p>font-weight: 500</p><p>line-height: 1.4</p></div></h5>
+          <h6><sim-type :type="h6"></sim-type><div class="demo-info"><p>font-size: .875rem</p>
+          <p>font-weight: 500</p><p>line-height: 1.4</p></div></h6>
+          <p class="library-label">Text</p>
           <p>This is a paragraph. Consectetur adipisicing elit <strong>this is emphasized</strong>. Laboriosam enim saepe a, vitae rerum sapiente velit, <a href="#">this is a link</a> incidunt at eligendi illo blanditiis quasi. Amet sint eveniet alias quasi <u>this is underlined</u> nulla ad optio, quod necessitatibus numquam pariatur eaque, vitae <em>this is italicized</em> illum illo molestiae qui corrupti eum tenetur voluptas. Ex voluptate velit dolor eligendi.</p>
           <p>Veritatis ad ea sint cum facilis, laborum quo ex impedit. <span class="text-highlight">Laborum sint velit dolores provident obcaecati doloribus natus officiis, repellendus aliquid nobis</span>, eum aperiam fugiat in incidunt itaque. Illo perferendis qui maxime exercitationem, dolore iusto. Odio ex nobis totam cupiditate adipisci, voluptas iusto maxime quas, eaque omnis enim temporibus.</p>
         </template>
 
         <template slot="html">
-          <pre v-highlightjs><code class="html">&lt;</code></pre>
+          <pre v-highlightjs><code class="html">&lt;h1>Headline h1&lt;/h1>
+&lt;h2>Headline h2&lt;/h2>
+&lt;h3>Headline h3&lt;/h3></code></pre>
         </template>
 
         <template slot="js">
