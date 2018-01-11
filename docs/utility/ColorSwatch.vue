4a5a5a5e10b04014f4b4fe4b4f146650feeb5779
Eric Thayer <eric.thayer594@gmail.com>
2018-01-11 13:10:43 -0700

    removed functions in methods

--- docs/utility/ColorSwatch.vue
+++ docs/utility/ColorSwatch.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div class="color-swatch">
-    <span class="color-swatch--example" :class="setTextColorClass()" :style="{'background': hexCode }">&nbsp;</span>
+    <span class="color-swatch--example" :class="textColorClass" :style="{'background': hexCode }"></span>
     <span class="color-swatch--name bg--lightest text--dark">
-      <b>{{ colorVariable }}</b><br/>
+      <b>{{ colorVariable }}</b>
       <small class="ghost">{{ hexCode }}</small>
     </span>
   </div>    
@@ -12,21 +12,19 @@
   export default {
     name: 'guide-color-swatch',
     props: ['hex-code', 'color-variable'],
-    methods: {
-      determineBrightness (hex) {
-        const r = parseInt(hex.slice(1, 3), 16)
-        const g = parseInt(hex.slice(3, 5), 16)
-        const b = parseInt(hex.slice(5, 7), 16)
+    computed: {
+      brightness () {
+        const r = parseInt(this.hexCode.slice(1, 3), 16)
+        const g = parseInt(this.hexCode.slice(3, 5), 16)
+        const b = parseInt(this.hexCode.slice(5, 7), 16)
         return (r * 299 + g * 587 + b * 114) / 1000
       },
-      setTextColorClass () {
-        return this.determineBrightness(this.hexCode) > 165 ? 'text--dark' : 'text--light'
+      textColorClass () {
+        return this.brightness > 165 ? 'text--dark' : 'text--light'
       },
-      setClasses () {
-        let classes = []
-        classes.push(this.setTextColorClass())
-        return classes.join(' ')
-      }
+    },
+    methods: {
+      
     }
   }
 </script>
