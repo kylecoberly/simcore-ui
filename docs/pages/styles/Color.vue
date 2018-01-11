f75bb04a3c2ae716f6d3355e5cd92fc831f8e067
Eric Thayer <eric.thayer594@gmail.com>
2018-01-11 16:53:10 -0700

    added color swatch functionality & tile styling

--- docs/pages/styles/Color.vue
+++ docs/pages/styles/Color.vue
@@ -17,73 +17,35 @@
               <div class="module--demo--item">
                 <ColorSwatch :hexCode="darkestHex" :color-variable="darkest"></ColorSwatch>
               </div>
-
-
               <div class="module--demo--item">
-                <div class="color-swatch bg--darkest text--light">                  
-                  <ColorSwatch :hexCode="darkestHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="darkest"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="darkerHex" :color-variable="darker"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--darker text--light">
-                  <ColorSwatch :hexCode="darkerHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="darker"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="darkHex" :color-variable="dark"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--dark text--light">
-                  <ColorSwatch :hexCode="darkHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="dark"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="darkGreyHex" :color-variable="darkGrey"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--dark-grey text--light">
-                   <ColorSwatch :hexCode="darkGreyHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="darkGrey"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="greyHex" :color-variable="grey"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--grey text--light">
-                  <ColorSwatch :hexCode="greyHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="grey"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="mediumGreyHex" :color-variable="mediumGrey"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--medium-grey text--light">
-                  <ColorSwatch :hexCode="mediumGreyHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="mediumGrey"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="lightGreyHex" :color-variable="lightGrey"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--light-grey">
-                  <ColorSwatch :hexCode="lightGreyHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="lightGrey"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="lighterGreyHex" :color-variable="lighterGrey"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--lighter-grey">                  
-                  <ColorSwatch :hexCode="lighterGreyHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="lighterGrey"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="light" :color-variable="light"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--light">                  
-                  <ColorSwatch :hexCode="lightHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="light"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="lighterHex" :color-variable="lighter"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--lighter">
-                  <ColorSwatch :hexCode="lighterHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="lighter"></ColorSwatchLabel>
-                </div>
-              </div>
-              <div class="module--demo--item">
-                <div class="color-swatch bg--lightest">
-                  <ColorSwatch :hexCode="lightestHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="lightest"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="lightestHex" :color-variable="lightest" class="bg--lightest"></ColorSwatch>
               </div>
             </div>
           </section>
@@ -92,109 +54,58 @@
             <h3 class="module-label">Brand Colors</h3>
             <div class="module module--demo module--demo--colors">
               <div class="module--demo--item">
-                <div class="color-swatch bg--blue text--light">
-                  <ColorSwatch :hexCode="blueHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="blue"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="blueHex" :color-variable="blue"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--blue-film text--light">
-                  <ColorSwatch :hexCode="blueFilmHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="blueFilm"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="blueFilmHex" :color-variable="blueFilm" :opacity="blueFilmHexAlpha"></ColorSwatch>
               </div>
               <div class="module--demo--item">
-                <div class="color-swatch bg--blue-film-heavy text--light">
-                  <ColorSwatch :hexCode="blueFilmHeavyHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="blueFilmHeavy"></ColorSwatchLabel>
-                </div>
+                <ColorSwatch :hexCode="blueFilmHeavyHex" :color-variable="blueFilmHeavy"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="redHex" :color-variable="red"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="redFilmHex" :color-variable="redFilm"  :opacity="redFilmHexAlpha"></ColorSwatch>
+              </div>              
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="orangeHex" :color-variable="orange"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="orangeFilmHex" :color-variable="orangeFilm"  :opacity="orangeFilmHexAlpha"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="yellowHex" :color-variable="yellow"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="yellowFilmHex" :color-variable="yellowFilm"  :opacity="yellowFilmHexAlpha"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="greenHex" :color-variable="green"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="greenFilmHex" :color-variable="greenFilm"  :opacity="greenFilmHexAlpha"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="aquaHex" :color-variable="aqua"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="aquaFilmHex" :color-variable="aquaFilm"  :opacity="aquaFilmHexAlpha"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="purpleHex" :color-variable="purple"></ColorSwatch>
+              </div>
+              <div class="module--demo--item">
+                <ColorSwatch :hexCode="purpleFilmHex" :color-variable="purpleFilm"  :opacity="purpleFilmHexAlpha"></ColorSwatch>
               </div>
             </div>
           </section>
-          <section class="module-section module-section--brand-colors">
+          <!-- <section class="module-section module-section--brand-colors">
             <div class="module module--demo module--demo--colors">
-              <div class="module--demo--item">
-                <div class="color-swatch bg--red text--light">
-                  <ColorSwatch :hexCode="redHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="red"></ColorSwatchLabel>
-                </div>
-              </div>
-              <div class="module--demo--item">
-                <div class="color-swatch bg--red-film text--light">
-                  <ColorSwatch :hexCode="redFilmHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="redFilm"></ColorSwatchLabel>
-                </div>
-              </div>
-              <div class="module--demo--item">
-                <div class="color-swatch bg--orange text--light">
-                  <ColorSwatch :hexCode="orangeHex"></ColorSwatch>
-                  <ColorSwatchLabel :color="orange"></ColorSwatchLabel>
-                </div>
-              </div>
+              
             </div>
-          </section>
+          </section> -->
         </template>
-
-        <!-- <template slot="scss">
-<pre v-highlightjs><code class="scss">
-  :root {
-    --darkest: #000;
-    --darker: #222;
-    --dark: #333;
-    --dark-grey: #444;
-    --grey: #565656;
-    --medium-grey: #777;
-    --light-grey: #ccc;
-    --lighter-grey: #ddd;
-    --light: #eee;
-    --lighter: #fafafa;
-    --lightest: #fff;
-
-    // colors
-    --red: #ca1e34;
-    --red-film: rgba(#ca1e34, .5);
-    --orange: #ff7042;
-    --orange-film: rgba(#ff7042, .5);
-    --yellow: #ffc929;
-    --yellow-film: rgba(#ffc929, .5);
-    --green: #9ccc66;
-    --green-film: rgba(#9ccc66, .5);
-    --aqua: #00c1d9;
-    --aqua-film: rgba(#00c1d9, .5);
-    --blue: #4A93B9;
-    --blue-film: rgba(#4A93B9, .5);
-    --blue-film-heavy: rgba(14, 103, 150, .9);
-    --purple: #ab47bd;
-    --purple-film: rgba(#ab47bd, .5);
-    --accent: var(--medium-grey);
-
-    // state
-    --action: var(--blue);
-    --active: var(--blue-film-heavy);
-    --focus: var(--aqua);
-
-    // feedback
-    --success: var(--green);
-    --danger: var(--red);
-    --warning: var(--orange);
-    --alert: var(--yellow);
-    --royal: var(--purple);
-
-    // accents
-    --shadow: rgba(#000, .8);
-    --twilight: rgba(#000, .5);
-    --dusk: rgba(#000, .3);
-    --shade: rgba(#000, .05);
-    --dust: rgba(#777, .05);
-    --edge: rgba(#777, .15);
-    --line: rgba(#777, .5);
-    --ghost: rgba(#808080, .5); // middle grey, middle opacity
-    --mist: rgba(#fff, .2);
-    --fog: rgba(#fff, .5);
-    --frost: rgba(#fff, .8);
-  }
-</code></pre>
-        </template> -->
 
         <template slot="js">
           <pre v-highlightjs><code class="javascript"></code></pre>
@@ -208,7 +119,6 @@
 <script>
   import Demobox from '../../utility/Demobox'
   import SimIconText from '../../../components/IconText'
-  import ColorSwatchLabel from '../../utility/ColorSwatchLabel'
   import ColorSwatch from '../../utility/ColorSwatch'
   
 export default {
@@ -216,50 +126,71 @@ export default {
   components: {
     Demobox,
     SimIconText,
-    ColorSwatchLabel,
     ColorSwatch,
   },
   data() {
     return {
       msg: 'Color',
-
       darkest: 'var(--darkest)',
-      darkestHex: '#ababab',
+      darkestHex: '#000000',
       darker: 'var(--darker)',
-      darkerHex: '#222',
+      darkerHex: '#222222',
       dark: 'var(--dark)',
-      darkHex: '#333',
+      darkHex: '#333333',
       darkGrey: 'var(--dark-grey)',
-      darkGreyHex: '#444',
+      darkGreyHex: '#444444',
       grey: 'var(--grey)',
       greyHex: '#565656',
       mediumGrey: 'var(--medium-grey)',
-      mediumGreyHex: '#777',
+      mediumGreyHex: '#777777',
       lightGrey: 'var(--light-grey)',
-      lightGreyHex: '#CCC',
+      lightGreyHex: '#CCCCCC',
       lighterGrey: 'var(--lighter-grey)',
-      lighterGreyHex: '#DDD',
+      lighterGreyHex: '#DDDDDD',
       light: 'var(--light)',
-      lightHex: '#EEE',
+      lightHex: '#EEEEEE',
       lighter: 'var(--lighter)',
       lighterHex: '#FAFAFA',
       lightest: 'var(--lightest)',
-      lightestHex: '#FFF',
-
+      lightestHex: '#FFFFFF',
       blue: 'var(--blue)',
       blueHex: '#4A93B9',
       blueFilm: 'var(--blue-film)',
-      blueFilmHex: 'rgba(#4A93B9, .5)',
+      blueFilmHex: '#4A93B9',
+      blueFilmHexAlpha: 0.5,
       blueFilmHeavy: 'var(--blue-film-heavy)',
-      blueFilmHeavyHex: 'rgba(#0E6796, .9)',
+      blueFilmHeavyHex: '#0E6796',
+      blueFilmHeavyHexAlpha: 0.9,
       red: 'var(--red)',
       redHex: '#CA1E34',
       redFilm: 'var(--red)',
-      redFilmHex: 'rgba(#CA1E34, .5)',
+      redFilmHex: '#CA1E34',
+      redFilmHexAlpha: 0.5,
       orange: 'var(--orange)',
       orangeHex: '#FF7042',
       orangeFilm: 'var(--orange-film)',
-      ornageFilmHex: 'rgba(#FF7042, .5)',
+      orangeFilmHex: '#FF7042',
+      orangeFilmHexAlpha: 0.5,
+      yellow: 'var(--yellow)',
+      yellowHex: '#FFC929',
+      yellowFilm: 'var(--yellow-film)',
+      yellowFilmHex: '#FFC929',
+      yellowFilmHexAlpha: 0.5,
+      green: 'var(--green)',
+      greenHex: '#9CCC66',
+      greenFilm: 'var(--green-film)',
+      greenFilmHex: '#9CCC66',
+      greenFilmHexAlpha: 0.5,
+      aqua: 'var(--aqua)',
+      aquaHex: '#00C1D9',
+      aquaFilm: 'var(--aqua-film)',
+      aquaFilmHex: '#00C1D9',
+      aquaFilmHexAlpha: 0.5,
+      purple: 'var(--purple)',
+      purpleHex: '#AB47BD',
+      purpleFilm: 'var(--purple-film)',
+      purpleFilmHex: '#AB47BD',
+      purpleFilmHexAlpha: 0.5,
 
     }
   },
--- docs/utility/ColorSwatch.vue
+++ docs/utility/ColorSwatch.vue
@@ -1,9 +1,10 @@
 <template lang="html">
   <div class="color-swatch">
-    <span class="color-swatch--example" :class="textColorClass" :style="{'background': hexCode }"></span>
+    <span class="color-swatch--example" :class="textColorClass" :style="{'background': colorCode }"></span>
     <span class="color-swatch--name bg--lightest text--dark">
       <b>{{ colorVariable }}</b>
-      <small class="ghost">{{ hexCode }}</small>
+      <small>{{ colorCode }}</small>
+      <small>{{ rgbColorCode }}</small>
     </span>
   </div>    
 </template>
@@ -11,7 +12,7 @@
 <script>
   export default {
     name: 'guide-color-swatch',
-    props: ['hex-code', 'color-variable'],
+    props: ['hex-code', 'opacity', 'color-variable'],
     computed: {
       brightness () {
         const r = parseInt(this.hexCode.slice(1, 3), 16)
@@ -22,9 +23,35 @@
       textColorClass () {
         return this.brightness > 165 ? 'text--dark' : 'text--light'
       },
+      colorCode() {
+        return this.opacity ? this.generateRGBAFromHexAndOpacity() : this.hexCode
+      },
+      rgbColorCode() {
+        if(this.hexCode) {
+          const rgbObject = this.hex2rgb(this.hexCode)
+          return `rgb(${rgbObject.r}, ${rgbObject.g}, ${rgbObject.b})`
+        } else {
+          return 'Missing HEX color code!'
+        }
+      },
     },
     methods: {
-      
+      hex2rgb(hex) {
+       if(hex) {
+         hex = hex.replace('#', '')
+         var r = (parseInt(hex.substring(0, 2), 16)|0)
+         var g = (parseInt(hex.substring(2, 4), 16)|0)
+         var b = (parseInt(hex.substring(4, 6), 16)|0)
+          return {r:r, g:g, b:b}
+        }
+      },
+      generateRGBAFromHexAndOpacity() {
+        if(this.opacity && this.hexCode) {
+          const rgbObject = this.hex2rgb(this.hexCode)
+          console.log(rgbObject)
+          return `rgba(${rgbObject.r}, ${rgbObject.g}, ${rgbObject.b}, ${this.opacity})`
+        }
+      },
     }
   }
 </script>
