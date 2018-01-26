b5b6288c787e8a7c6773c3db1c605ce22f772909
Eric Thayer <eric.thayer594@gmail.com>
2018-01-26 11:14:54 -0700

    added Tooltip demo details

--- docs/pages/components/Tooltip.vue
+++ docs/pages/components/Tooltip.vue
@@ -2,26 +2,51 @@
   <article>
       <h2>{{ msg }}</h2>
 
-      <demobox :open="true">
+      <demobox>
         <template slot="intro">
           Normal / Default
         </template>
         <template slot="view">
           <p>
             Never give up
-            <sim-tooltip pop="up">
-              <span slot="text">It always seems impossible until it's done.</span>
-            </sim-tooltip>
+            <SimTooltip pop="up">
+              <span slot="text">
+                It always seems impossible until it's done.
+              </span>
+            </SimTooltip>
           </p>
           <p>
             Never surrender
-            <sim-tooltip pop="down">
+            <SimTooltip pop="down">
               <span slot="text">When something really matters, you should never give up or give in.</span>
-            </sim-tooltip>
+            </SimTooltip>
           </p>
         </template>
         <template slot="html">
-          <pre v-highlightjs><code class="html">&lt;sim-icontext icon="fa-star" text="Getting Started" /></code></pre>
+          <pre v-highlightjs><code class="html">&lt;SimTooltip pop="up">
+    &lt;span slot="text">It always seems impossible until it's done.&lt;/span>
+  &lt;/SimTooltip>
+
+  &lt;SimTooltip pop="down">
+    &lt;span slot="text">When something really matters, you should never give up or give in.&lt;/span>
+  &lt;/SimTooltip></code></pre>
+        </template>
+        <template slot="js">
+          <pre v-highlightjs><code class="javascript">props: {
+  pop: {
+    type: String,
+    default: 'up',
+  },
+  shift: {
+    type: String,
+    default: 'none',
+  },
+},
+computed: {
+  popTo() {
+    return `sim-tooltip__bubble--${this.pop}`
+  },
+},</code></pre>
         </template>
       </demobox>
 
