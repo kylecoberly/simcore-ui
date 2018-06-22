a5f3d9f2970bb7fe0e824a72540955c76a83272d
Chad Beninati <chad.beninati@gmail.com>
2018-06-22 11:24:48 -0600

    Load autocomplete for large filter sets and a list for smaller sets
    - also updated 'Autocomplete' to handle thousands of records

--- docs/pages/components/FilterBy.vue
+++ docs/pages/components/FilterBy.vue
@@ -1,26 +1,37 @@
 <template lang="html">
   <article>
-      <h2>{{ msg }}</h2>
+    <h2>{{ msg }}</h2>
 
-      <demobox>
-        <template slot="intro">
-          Normal / Default
-        </template>
-        <template slot="view">
-          <p>FilterBy component</p>
-        </template>
-        <template slot="html">
-          <pre v-highlightjs><code class="html"></code></pre>
-        </template>
-        <template slot="js">
-          <pre><code class="javascript"></code></pre>
-        </template>
-      </demobox>
+    <demobox>
+      <template slot="intro">
+        Normal / Default
+      </template>
+      <template slot="view">
+        <SimFilterBy
+          :list="this.dummyData"
+          label="Departments"
+          type="departments"
+        ></SimFilterBy>
+        <SimFilterBy
+          :list="this.bigDummyData"
+          label="Professional Titles"
+          type="professional_titles"
+        ></SimFilterBy>
+      </template>
+      <template slot="html">
+        <pre v-highlightjs><code class="html"></code></pre>
+      </template>
+      <template slot="js">
+        <pre><code class="javascript"></code></pre>
+      </template>
+    </demobox>
 
   </article>
 </template>
 
 <script>
+  import dummyData from '../../../components/Filters/dumData'
+
   import Demobox from '../../utility/Demobox'
   import SimFilterBy from '../../../components/FilterBy'
 
@@ -33,6 +44,8 @@
     data() {
       return {
         msg: 'FilterBy',
+        dummyData: _.take(dummyData(), 20),
+        bigDummyData: dummyData(),
       }
     },
     computed: {
