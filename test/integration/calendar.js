de0ae6a432cbab775644019ca43da416096f2fcc
Kyle Coberly <kyle.coberly@gmail.com>
2018-07-12 13:35:33 -0600

    getCurrentMonth and componentClasses have tests, current is refactored

--- test/integration/calendar.js
+++ test/integration/calendar.js
@@ -29,6 +29,7 @@ describe('Calendar', () => {
       it('returns a default day if none is defined', () => {
         this.component = shallowMount(Calendar, {
           propsData: {
+            date: moment('1970-01-01'),
             activeMoment: moment('1970-01-01'),
           },
         })
@@ -64,6 +65,77 @@ describe('Calendar', () => {
           someValue: 'Some Value',
         })
       })
+    })
+  })
+  describe('#componentClasses', () => {
+    before(() => {
+      this.component = shallowMount(Calendar, {
+        propsData: {
+          activeMoment: moment('1970-01-01'),
+          monthDays: this.monthDays,
+          date: moment('1970-02-01'),
+          dateFormat: 'YYYY-MM-DD',
+        },
+      })
+    })
+    it('returns a simple class string', () => {
+      assert.equal(this.component.vm.componentClasses, 'is-instructor-context')
+    })
+    it('returns a class string with the current month when its the current month', () => {
+      this.component.setProps({
+        date: moment('1970-01-01'),
+        showExpandedWeek: false,
+        bubbleIsOpen: false,
+      })
+      assert.equal(this.component.vm.componentClasses, 'is-instructor-context is-current-month')
+    })
+    it('returns a class string with an expansion when showExpandedWeek is true', () => {
+      this.component.setProps({
+        date: moment('1970-02-01'),
+        showExpandedWeek: true,
+        bubbleIsOpen: false,
+      })
+      assert.equal(this.component.vm.componentClasses, 'is-instructor-context is-expanded')
+    })
+    it('returns a class string with an expansion when bubbleIsOpen is true', () => {
+      this.component.setProps({
+        date: moment('1970-02-01'),
+        showExpandedWeek: false,
+        bubbleIsOpen: true,
+      })
+      assert.equal(this.component.vm.componentClasses, 'is-instructor-context is-expanded')
+    })
+    it('returns a class string with an expansion and the current when both match', () => {
+      this.component.setProps({
+        date: moment('1970-01-01'),
+        showExpandedWeek: true,
+        bubbleIsOpen: false,
+      })
+      assert.equal(this.component.vm.componentClasses, 'is-instructor-context is-current-month is-expanded')
+    })
+  })
+  describe('#isCurrentMonth', () => {
+    before(() => {
+      this.component = shallowMount(Calendar, {
+        propsData: {
+          activeMoment: moment('1970-01-01'),
+          monthDays: this.monthDays,
+          dateFormat: 'YYYY-MM-DD',
+          date: moment('1970-01-01'),
+        },
+      })
+    })
+    it('returns true when it is the current month', () => {
+      this.component.setProps({
+        date: moment('1970-01-01'),
+      })
+      assert.ok(this.component.vm.isCurrentMonth)
+    })
+    it('returns false when it isn\'t the current month', () => {
+      this.component.setProps({
+        date: moment('1970-02-01'),
+      })
+      assert.ok(!this.component.vm.isCurrentMonth)
     })
   })
 })
