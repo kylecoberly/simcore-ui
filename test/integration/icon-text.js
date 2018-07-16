badd61c5229e572e356b8a6e3fd22883e1c347ea
Kyle Coberly <kyle.coberly@gmail.com>
2018-07-16 14:07:00 -0600

    Adding integration test for icon text

---
+++ test/integration/icon-text.js
@@ -0,0 +1,34 @@
+/* global describe, before, it */
+/* eslint */
+import { mount } from '@vue/test-utils'
+import assert from 'assert'
+import IconText from '../../components/IconText'
+
+describe('IconText', () => {
+  before(() => {
+    mount(IconText, {
+      propsData: {
+        icon: '',
+        iconType: '',
+        text: '',
+        circle: false,
+        round: false,
+        square: false,
+      },
+    })
+  })
+  describe('#render', () => {
+    describe('simple', () => {
+      before(() => {
+        this.component = mount(IconText, {
+          propsData: {
+            text: 'Simple',
+          },
+        })
+      })
+      it('Renders basic text', () => {
+        assert.equal(this.component.text(), 'Simple')
+      })
+    })
+  })
+})
