5461ae834512e9e59df8a05a31ac58efc25b12f9
Kyle Coberly <kyle.coberly@gmail.com>
2018-07-18 17:20:23 -0600

    Refactored Calendar Header

--- components/CalendarHeader.vue
+++ components/CalendarHeader.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="sim-calendar--header">
     <div class="sim-calendar--header--controls sim-calendar--header--controls--months">
-      <span class="load-previous-month" @click="loadPreviousMonth">
+      <span class="load-previous-month" @click="$emit('loadPreviousMonth')">
         <SimIconText icon="fa-arrow-left fa-fw"></SimIconText>
       </span>
-      <span class="load-current-month" @click="setTheActiveDateToToday">
+      <span class="current-month">
         {{ displayDate }}
       </span>
-      <span class="load-next-month" @click="loadNextMonth">
+      <span class="load-next-month" @click="$emit('loadNextMonth')">
         <SimIconText icon="fa-arrow-right fa-fw"></SimIconText>
       </span>
     </div>
     <div class="sim-calendar--header--mode" v-if="canScheduleEvents">
-      <SimSwitch :context="context" @click="toggleContext" left-label="My Availability" right-label="Schedule Events"/>
+      <SimSwitch :value="context" @change="toggleContext" left-label="My Availability" right-label="Schedule Events"/>
     </div>
     <div class="sim-calendar--header--controls sim-calendar--header--controls--days">
-      <span class="load-previous-day" @click="loadPrevDay">
+      <span class="load-previous-day" @click="$emit('loadPreviousDay')">
         <SimIconText icon="fa-arrow-left fa-fw"></SimIconText>
       </span>
-      <span class="load-today" @click="setTheActiveDateToToday">
+      <span class="load-today" @click="$emit('loadToday')">
         today
       </span>
-      <span class="load-next-day" @click="loadNextDay">
+      <span class="load-next-day" @click="$emit('loadNextDay')">
         <SimIconText icon="fa-arrow-right fa-fw"></SimIconText>
       </span>
     </div>
@@ -37,28 +37,23 @@
       SimIconText,
       SimSwitch
     },
+    data(){
+      return {
+        context: false,
+      }
+    },
     props: {
-      displayDate: String,
-      context: Boolean,
+      selectedDate: Object,
       canScheduleEvents: Boolean,
     },
+    computed: {
+      displayDate(){
+        return this.selectedDate.format('MMMM')
+      }
+    },
     methods: {
-      loadPrevDay(){
-        this.$emit('loadPrevDay')
-      },
-      setTheActiveDateToToday(){
-        this.$emit('setTheActiveDateToToday')
-      },
-      loadNextDay(){
-        this.$emit('loadNextDay')
-      },
-      loadPreviousMonth(){
-        this.$emit('loadPreviousMonth')
-      },
-      loadNextMonth(){
-        this.$emit('loadNextMonth')
-      },
       toggleContext(){
+        this.context = !this.context
         this.$emit('toggleContext')
       },
     },
