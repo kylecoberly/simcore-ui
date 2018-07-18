421efa4013b7c2bba6a8ae85776a7acb6d51f45e
Kyle Coberly <kyle.coberly@gmail.com>
2018-07-18 17:25:51 -0600

    Refactored Calendar header

--- test/integration/calendar-header.js
+++ test/integration/calendar-header.js
@@ -9,40 +9,44 @@ describe('CalendarHeader', () => {
     this.component = mount(CalendarHeader, {
       propsData: {
         displayDate: 'Friday, July 13th',
-        context: false,
         canScheduleEvents: true,
       },
     })
   })
   it('displays the date', () => {
-    assert.equal(this.component.find('.load-current-month').text(), 'Friday, July 13th')
+    assert.equal(this.component.find('.current-month').text(), 'Friday, July 13th')
   })
   it('goes to the previous month', () => {
     this.component.find('.load-previous-month').trigger('click')
-    assert.deepEqual(this.component.emitted().loadPreviousMonth[0], {})
-  })
-  it('goes to the current month', () => {
-    this.component.find('.load-current-month').trigger('click')
-    assert.deepEqual(this.component.emitted().setTheActiveDateToToday[0], {})
+    assert.ok(this.component.emitted().loadPreviousMonth[0])
   })
   it('goes to the next month', () => {
     this.component.find('.load-next-month').trigger('click')
-    assert.deepEqual(this.component.emitted().loadNextMonth[0], {})
+    assert.ok(this.component.emitted().loadNextMonth[0])
   })
   it('goes to the previous day', () => {
     this.component.find('.load-previous-day').trigger('click')
-    assert.deepEqual(this.component.emitted().loadPrevDay[0], {})
+    assert.ok(this.component.emitted().loadPrevDay[0])
   })
   it('goes to the current day', () => {
     this.component.find('.load-today').trigger('click')
-    assert.deepEqual(this.component.emitted().setTheActiveDateToToday[0], {})
+    assert.ok(this.component.emitted().setTheActiveDateToToday[0])
   })
   it('goes to the next day', () => {
     this.component.find('.load-next-day').trigger('click')
-    assert.deepEqual(this.component.emitted().loadNextDay[0], {})
+    assert.ok(this.component.emitted().loadNextDay[0])
+  })
+  it('toggles between availability and scheduling events', () => {
+    this.component.find('.sim-switch input').trigger('click')
+    assert.ok(this.component.emitted().toggleContext[0])
   })
   it('hides the toggle if the user can\'t schedule events', () => {
-    this.component.setData({ canScheduleEvents: false })
-    assert.ok(!this.component.find('.sim-calendar--header--mode').exists())
+    const component = mount(CalendarHeader, {
+      propsData: {
+        displayDate: 'Friday, July 13th',
+        canScheduleEvents: false,
+      },
+    })
+    assert.ok(!component.find('.sim-calendar--header--mode').exists())
   })
 })
