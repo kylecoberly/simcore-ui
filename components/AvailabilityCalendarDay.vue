73087a93df20f3b253ba05176f01f3e665d9e42b
Kyle Coberly <kyle.coberly@gmail.com>
2018-08-10 10:04:51 -0600

    Refactoring calenday body and days to remove repetition

---
+++ components/AvailabilityCalendarDay.vue
@@ -0,0 +1,103 @@
+<template>
+  <CalendarDay
+    :day="day"
+    @toggleExpandedWeek="$emit('toggleExpandedWeek')"
+  >
+    <TimeLines v-if="showTimelines"
+      slot="timelines"
+      class="sim-calendar--grid--day--timelines"
+      :showHalfHourTicks="false"
+      @createTimeBlock="createTimeBlock"
+    />
+    <div
+      class="local--day--blocks local--day--event-blocks"
+      slot="events"
+    ></div>
+    <div
+      v-if="!isBeforeToday"
+      class="local--day--blocks local--day--time-blocks"
+      slot="availabilities"
+    >
+      <template v-for="(block, index) in availabilities">
+        <AvailabilityBlock
+          theme="available"
+          :key="index"
+          :block="block"
+          @removeTimeBlock="removeTimeBlock(index)"
+          @updateTimeBlock="updateTimeBlock"
+        />
+      </template>
+    </div>
+  </CalendarDay>
+</template>
+
+<script>
+  import TimeLines from './TimeLines'
+  import AvailabilityBlock from './AvailabilityBlock'
+  import CalendarDay from './CalendarDay'
+
+  export default {
+    components: {
+      TimeLines,
+      AvailabilityBlock,
+      CalendarDay,
+    },
+    props: {
+      day: Object,
+      availabilities: Array,
+      showExpandedWeek: Boolean,
+    },
+    data() {
+      return {
+        timeBlockDefaultDuration: 1,
+      }
+    },
+    computed: {
+      dateService() {
+        return this.$store.state.services.date
+      },
+      isSelected() {
+        return this.dateService.selectedDate.isSame(this.day, 'day')
+      },
+      showTimelines() {
+        return this.isSelected && this.showExpandedWeek
+      },
+      today() {
+        return this.dateService.today
+      },
+      isBeforeToday() {
+        return this.today.isAfter(this.day, 'day')
+      },
+    },
+    methods: {
+      createTimeBlock(hour) {
+        const newBlock = {
+          startTime: hour,
+          duration: this.timeBlockDefaultDuration,
+        }
+        const availabilities = [...this.availabilities]
+        availabilities.push(newBlock)
+        this.updateAvailabilities(this.day, availabilities)
+      },
+      updateAvailabilities(date, availabilities) {
+        this.$emit('updateAvailabilities', date, availabilities)
+      },
+      removeTimeBlock(index) {
+        console.log('hey', index)
+        const availabilities = [...this.availabilities]
+        availabilities.splice(index, 1)
+        this.updateAvailabilities(this.day, availabilities)
+      },
+      updateTimeBlock(index, value) {
+        const availabilities = [...this.availabilities]
+        availabilities[index] = value
+        this.updateAvailabilities(this.day, availabilities)
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+  @import '../styles/calendar-day';
+</style>
+
--- components/EventCalendarDay.vue
+++ components/EventCalendarDay.vue
@@ -1,50 +1,56 @@
 <template>
-  <div class="sim-calendar-day sim-calendar--grid--day"
-    :class="dayClasses"
+  <CalendarDay
+    :day="day"
+    @toggleExpandedWeek="$emit('toggleExpandedWeek')"
   >
-    <div class="local--day">
-      <SimTimeLines v-if="showTimelines"
-        class="sim-calendar--grid--day--timelines"
-        :showHalfHourTicks="false"
+    <TimeLines v-if="showTimelines"
+      slot="timelines"
+      class="sim-calendar--grid--day--timelines"
+      :showHalfHourTicks="false"
+    />
+    <div
+      class="local--day--blocks local--day--event-blocks"
+      slot="events"
+    ></div>
+    <div
+      class="local--day--blocks local--day--pending-blocks"
+      slot="pending-events"
+    >
+      <PendingEventBlock v-if="pendingEvent"
+        :block="pendingEvent"
+        @updatePosition="updateBlockPosition"
+        @updatePendingEvent="updatePendingEvent"
+        @clearPendingEvent="$emit('clearPendingEvent')"
       />
-      <div class="sim-calendar--grid--date">{{ day.format('D') }}</div>
-      <div class="local--day--blocks local--day--event-blocks"></div>
-      <div class="local--day--blocks local--day--pending-blocks">
-        <PendingEventBlock v-if="pendingEvent"
-          :block="pendingEvent"
-          @updatePosition="updateBlockPosition"
-          @updatePendingEvent="updatePendingEvent"
-          @clearPendingEvent="$emit('clearPendingEvent')"
+    </div>
+    <div
+      v-if="!isBeforeToday"
+      class="local--day--blocks local--day--aggregate-blocks"
+      slot="aggregates"
+    >
+      <div v-for="(block, index) in availabilities"
+        slot="aggregates"
+      >
+        <EventAvailabilityBlock
+          :index="index"
+          :block="block"
+          @click.native="createPendingEvent(block)"
         />
       </div>
-      <div v-if="!isBeforeToday" class="local--day--blocks local--day--aggregate-blocks">
-        <template v-for="(block, index) in availabilities">
-          <EventAvailabilityBlock
-            :index="index"
-            :block="block"
-            @click.native="createPendingEvent(block)"
-          />
-        </template>
-      </div>
-      <div class="sim-calendar--grid--tools">
-        <span @click="toggleExpandedWeek">
-          <SimIconText :icon="expandIcon" icon-type="svg"></SimIconText>
-        </span>
-      </div>
     </div>
-  </div>
+  </CalendarDay>
 </template>
 
 <script>
-  import SimIconText from './IconText'
-  import SimTimeLines from './TimeLines'
+  import CalendarDay from './CalendarDay'
+  import TimeLines from './TimeLines'
   import EventAvailabilityBlock from './EventAvailabilityBlock'
   import PendingEventBlock from './PendingEventBlock'
 
   export default {
     components: {
-      SimIconText,
-      SimTimeLines,
+      CalendarDay,
+      TimeLines,
       EventAvailabilityBlock,
       PendingEventBlock,
     },
@@ -52,65 +58,26 @@
       day: Object,
       availabilities: [Array, Object],
       showExpandedWeek: Boolean,
-      pendingEvent: Object,
+      pendingEvent: Object, // Needs to be an array, could be multiple
     },
     computed: {
       dateService() {
         return this.$store.state.services.date
       },
-      today() {
-        return this.dateService.today
-      },
       isSelected() {
         return this.dateService.selectedDate.isSame(this.day, 'day')
-      },
-      isInActiveWeek() {
-        return this.dateService.selectedDate.isSame(this.day, 'week')
-      },
-      isBeforeToday() {
-        return this.today.isAfter(this.day, 'day')
-      },
-      dayClasses() {
-        const dayOfWeek = this.day.day()
-        const classes = [`day-${dayOfWeek}`]
-
-        const isBeforeToday = this.day.isBefore(this.today, 'day')
-        const isToday = this.day.isSame(this.today, 'day')
-        const isAfterToday = this.day.isAfter(this.today, 'day')
-        if (isBeforeToday) {
-          classes.push('is-before-today')
-        } else if (isToday) {
-          classes.push('is-today')
-        } else if (isAfterToday) {
-          classes.push('is-after-today')
-        }
-
-        const dayOfWeekClassName = [0, 6].includes(dayOfWeek) ? 'is-weekend' : 'is-weekday'
-        classes.push(dayOfWeekClassName)
-
-        if (this.isInActiveWeek) {
-          classes.push('is-in-active-week')
-        }
-        if (this.isSelected) {
-          classes.push(this.selectedClass || 'is-selected')
-        }
-
-        return classes.join(' ')
-      },
-      showTimeBlockControls() {
-        return this.showExpandedWeek && this.isInActiveWeek
       },
       showTimelines() {
         return this.isSelected && this.showExpandedWeek
       },
-      expandIcon() {
-        return this.showExpandedWeek && this.isInActiveWeek ? '#icon--control--contract' : '#icon--control--expand'
+      today() {
+        return this.dateService.today
+      },
+      isBeforeToday() {
+        return this.today.isAfter(this.day, 'day')
       },
     },
     methods: {
-      toggleExpandedWeek() {
-        this.$emit('toggleExpandedWeek')
-      },
       createPendingEvent(block) {
         block.day = this.day
         this.$emit('createPendingEvent', block)
