676b373e0e74569f396d0e37f60909c8ed6e45f0
Kyle Coberly <kyle.coberly@gmail.com>
2018-08-10 16:54:32 -0600

    Starting test refactor

---
+++ test/unit/components/calendar-body.js
@@ -0,0 +1,28 @@
+/* global describe, beforeEach, it */
+/* eslint */
+import { shallowMount } from '@vue/test-utils'
+import assert from 'assert'
+import dayjs from 'dayjs'
+import CalendarBody from '../../components/CalendarBody'
+
+xdescribe('CalendarBody', () => {
+  beforeEach(() => {
+    this.component = shallowMount(CalendarBody, {
+      propsData: {
+        selectedDate: dayjs('2018-07-01'),
+      },
+    })
+  })
+  it('renders', () => {
+    assert.ok(this.component.find('*').exists())
+  })
+  it('#startOffset')
+  it('#endOffset')
+  it('#daysInCurrentMonth')
+  it('#toggleExpandedWeek')
+  it('#setDate')
+  it('#updateAvailabilities')
+  it('#getAvailabilitiesForDay')
+  it('#isSelected')
+  it('#inActiveWeek')
+})
