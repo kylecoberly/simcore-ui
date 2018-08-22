9cfe4e255dbcf61e183fa7d8c0bd3c7621bb87f8
Kyle Coberly <kyle.coberly@gmail.com>
2018-08-22 11:39:09 -0600

    Bug fixes

---
+++ components/TimeBlockSpecificAvailability.vue
@@ -0,0 +1,31 @@
+<template>
+  <TimeBlock theme="specific" :style="blockStyles" />
+</template>
+
+<script>
+  import TimeBlock from './TimeBlock'
+
+  export default {
+    extends: TimeBlock,
+    components: {
+      TimeBlock,
+    },
+    props: {
+      block: Object,
+    },
+    computed: {
+      blockStyles() {
+        const styles = []
+
+        styles.push(`--start: ${this.block.startTime - this.timeShiftOffset}`)
+        styles.push(`--duration: ${this.block.duration}`)
+        if (this.maximumDuration < 24) {
+          styles.push(`--segment-size: ${this.segmentSize}`)
+        }
+
+        return styles.join(';')
+      },
+    },
+  }
+</script>
+
